0c74fd3b081d6241e6cbf8cdd6e3d171cf66a50d
Haibao Tang <tanghaibao@gmail.com>
2011-03-08 22:08:03 -0500

    add algorithms.graph

---
+++ algorithms/graph.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+Wrapper for the common graph algorithms. Common usages are:
+
+>>> edges = [(1, 2), (2, 3), (4, 5)]
+>>> g = nx.DiGraph(edges)
+>>> c = weakly_connected_components(g)
+>>> print c
+[[1, 2, 3], [4, 5]]
+>>> sub = g.subgraph(c[0])
+>>> topological_sort(sub)
+[1, 2, 3]
+"""
+
+import networkx as nx
+
+from networkx.algorithms.dag import topological_sort
+from networkx.algorithms.components.weakly_connected import weakly_connected_components
+from networkx.algorithms.components.connected import connected_components
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()

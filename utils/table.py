8da78ea3dff0158f2d1eb4bc235999ba0c8d3861
Haibao Tang <tanghaibao@gmail.com>
2011-06-22 14:57:46 -0400

    add transpose option to tabulate

--- utils/table.py
+++ utils/table.py
@@ -13,7 +13,7 @@ def get_table():
     return table
 
 
-def tabulate(d, key_fun=None):
+def tabulate(d, transpose=False, key_fun=None):
     """
     d is a dictionary, keyed by tuple(A, B).
     Goal is to put A in rows, B in columns, report data in table form.
@@ -26,15 +26,28 @@ def tabulate(d, key_fun=None):
     1    3    4
     2    5    0
     -----------
+    >>> d = {(1,'a'):3, (1,'b'):4, (2,'a'):5, (2,'b'):0}
+    >>> print tabulate(d, transpose=True)
+    ===========
+    o    1    2
+    -----------
+    a    3    5
+    b    4    0
+    -----------
     """
     pairs = d.keys()
     rows, cols = zip(*pairs)
+    if transpose:
+        rows, cols = cols, rows
+
     rows = sorted(set(rows))
     cols = sorted(set(cols))
     header = ["o"] + list(cols)
     table = []
     for r in rows:
         combo = [(r, c) for c in cols]
+        if transpose:
+            combo = [(c, r) for (r, c) in combo]
         data = [d[x] for x in combo]
         if key_fun:
             data = [key_fun(x) for x in data]

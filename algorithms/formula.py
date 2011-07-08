508fd8f5b5e74a4738e25fb56c142cadc9d0a534
Haibao Tang <tanghaibao@gmail.com>
2011-07-07 20:26:42 -0400

    add velvet ram estimator

--- algorithms/formula.py
+++ algorithms/formula.py
@@ -8,6 +8,11 @@ Some math formula for various calculations
 import sys
 
 from math import log, exp
+from optparse import OptionParser
+
+from jcvi.utils.cbook import human_size
+from jcvi.apps.base import ActionDispatcher, debug
+debug()
 
 
 def jukesCantorD(p, L=100):
@@ -38,6 +43,50 @@ def jukesCantorP(D):
     return p
 
 
+def main():
+
+    actions = (
+        ('velvet', 'calculate velvet memory requirement'),
+            )
+    p = ActionDispatcher(actions)
+    p.dispatch(globals())
+
+
+def velvet(args):
+    """
+    %prog velvet readsize genomesize numreads K
+
+    Calculate velvet memory requirement.
+    <http://seqanswers.com/forums/showthread.php?t=2101>
+
+    Ram required for velvetg = -109635 + 18977*ReadSize + 86326*GenomeSize +
+    233353*NumReads - 51092*K
+
+    Read size is in bases.
+    Genome size is in millions of bases (Mb)
+    Number of reads is in millions
+    K is the kmer hash value used in velveth
+    """
+    p = OptionParser(velvet.__doc__)
+    opts, args = p.parse_args(args)
+
+    if len(args) != 4:
+        sys.exit(not p.print_help())
+
+    readsize, genomesize, numreads, K = [int(x) for x in args]
+    ram = -109635 + 18977 * readsize + 86326 * genomesize + \
+            233353 * numreads - 51092 * K
+    print >> sys.stderr, "ReadSize: {0}".format(readsize)
+    print >> sys.stderr, "GenomeSize: {0}Mb".format(genomesize)
+    print >> sys.stderr, "NumReads: {0}M".format(numreads)
+    print >> sys.stderr, "K: {0}".format(K)
+
+    ram = human_size(ram * 1000, a_kilobyte_is_1024_bytes=True)
+    print >> sys.stderr, "RAM usage: {0} (MAXKMERLENGTH=31)".format(ram)
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+
+    main()

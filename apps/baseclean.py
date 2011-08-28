671eea20541c75f2a4a14ddffc4ad85a5c7cdf6e
Haibao Tang <tanghaibao@gmail.com>
2011-08-28 12:27:58 -0400

    trimmomatic-0.14 handles input with different qual-encodings; update apps.baseclean.trim() to use version 0.14 and remove the qual conversions.

--- apps/baseclean.py
+++ apps/baseclean.py
@@ -11,7 +11,7 @@ import sys
 
 from optparse import OptionParser
 
-from jcvi.formats.fastq import guessoffset, convert
+from jcvi.formats.fastq import guessoffset
 from jcvi.apps.command import JAVAPATH
 from jcvi.apps.base import ActionDispatcher, debug, set_grid, download, sh
 debug()
@@ -29,15 +29,17 @@ def main():
 
 def trim(args):
     """
-    %prog trim fastqfiles 
+    %prog trim fastqfiles
 
     Trim reads using TRIMMOMATIC. If two fastqfiles are given, then it invokes
     the paired reads mode. See manual:
 
     <http://www.usadellab.org/cms/index.php?page=trimmomatic>
     """
+    TrimVersion = tv = "0.14"
+    TrimJar = "trimmomatic-{0}.jar".format(tv)
     p = OptionParser(trim.__doc__)
-    p.add_option("--path", default="/home/htang/bin/trimmomatic-0.13.jar",
+    p.add_option("--path", default=op.join("~/bin", TrimJar),
             help="Path to trimmomatic [default: %default]")
     set_grid(p)
 
@@ -48,14 +50,16 @@ def trim(args):
 
     path = opts.path
     url = \
-    "http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.13.zip"
+    "http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-{0}.zip"\
+    .format(tv)
 
     if not op.exists(path):
         path = download(url)
-        if not op.exists("Trimmomatic-0.13"):
+        TrimUnzipped = "Trimmomatic-" + tv
+        if not op.exists(TrimUnzipped):
             sh("unzip " + path)
         os.remove(path)
-        path = "Trimmomatic-0.13/trimmomatic-0.13.jar"
+        path = op.join(TrimUnzipped, TrimJar)
 
     assert op.exists(path)
 
@@ -66,13 +70,8 @@ def trim(args):
     assert op.exists("adapters.fasta"), \
         "Please place the illumina adapter sequence in `adapters.fasta`"
 
-    # Trimmomatic only accepts offset-64, convert if needed
-    for i, fastqfile in enumerate(args):
-        offset = guessoffset([fastqfile])
-        if offset != 64:
-            newfastqfile = fastqfile.rstrip(".gz").rsplit(".", 1)[0] + ".b64.fastq.gz"
-            convert([fastqfile, newfastqfile])
-            args[i] = newfastqfile
+    offset = guessoffset([args[0]])
+    phredflag = " -phred{0}".format(offset)
 
     cmd = JAVAPATH("java-1.6.0")
     cmd += " -Xmx4g -cp {0} org.usadellab.trimmomatic".format(path)
@@ -80,12 +79,14 @@ def trim(args):
     pairs = ".pairs.fastq.gz"
     if len(args) == 1:
         cmd += ".TrimmomaticSE"
+        cmd += phredflag
         fastqfile, = args
         prefix = fastqfile.split(".")[0]
         frags1 = prefix + frags
         cmd += " {0}".format(" ".join((fastqfile, frags1)))
     else:
         cmd += ".TrimmomaticPE"
+        cmd += phredflag
         fastqfile1, fastqfile2 = args
         prefix1 = fastqfile1.split(".")[0]
         prefix2 = fastqfile2.split(".")[0]

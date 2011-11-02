8f48e132c27104345cbda237fca6d3d9d3b9130f
Haibao Tang <tanghaibao@gmail.com>
2011-11-02 13:31:36 -0400

    improve assembly.scaffoldQC for plotting alignments of evidences to scaffolds

--- assembly/scaffoldQC.py
+++ assembly/scaffoldQC.py
@@ -1,32 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-"""
-%prog scaffold.sizes synteny.bed physicalmap.bed
-
-As evaluation of scaffolding, visualize external line of evidences:
-* Plot synteny to an external genome
-* Plot alignments to physical map
-* Plot alignments to genetic map (TODO)
-
-For example, physicalmap.bed can look like
-scf7180014794474 104 104 ctg991
-
-and will plot a dot in the dot plot in the corresponding location
-the plots are one contig/scaffold per plot, but descend down the list in
-scaffolds.sizes, until the contig is smaller than the cutoff
-"""
-
 import sys
 import logging
 
 from optparse import OptionParser
 
+from jcvi.formats.blast import Blast
+from jcvi.formats.bed import Bed, BedLine
+from jcvi.formats.sizes import Sizes
 from jcvi.graphics.base import plt, Rectangle, _, human_size_formatter
 from jcvi.utils.cbook import thousands
-from jcvi.formats.bed import Bed
-from jcvi.formats.sizes import Sizes
-from jcvi.apps.base import debug
+from jcvi.apps.base import ActionDispatcher, debug
 debug()
 
 
@@ -89,27 +74,97 @@ def plot_one_scaffold(scaffoldID, scafsize, beds, imagename):
     logging.debug("saving image to `{0}`".format(imagename))
 
 
-if __name__ == '__main__':
+def main():
 
-    p = OptionParser(__doc__)
-    p.add_option("--cutoff", dest="cutoff", default=5 * 1e6,
+    actions = (
+        ('bed', 'make evidence alignments in the appropriate bed formats'),
+        ('plot', 'plot the alignment of the scaffold to other evidences'),
+            )
+    p = ActionDispatcher(actions)
+    p.dispatch(globals())
+
+
+def bed(args):
+    """
+    %prog bed oldbedfile blastfile newbedfile
+
+    Make evidence alignments in the appropriate bed formats.
+
+    `newbedfile` contains lines that look like,
+
+    scfxxxxxx 100 200 ctg3234 (physical map contig)
+    scfxxxxxx 100 200 RAPAscfxxxxx (scaffold in a related organism)
+
+    which really involves converting blast result, associating to a different
+    scaffold or molecule through `oldbedfile`, and output `newbedfile`.
+    """
+    p = OptionParser(bed.__doc__)
+    opts, args = p.parse_args(args)
+
+    if len(args) != 3:
+        sys.exit(not p.print_help())
+
+    bedfile, blastfile, newbedfile = args
+    border = Bed(bedfile).order
+    fw = open(newbedfile, "w")
+    nbed = Bed()
+
+    blast = Blast(blastfile)
+    for b in blast.iter_line():
+        query, subject = b.query, b.subject
+        sstart, sstop = b.sstart, b.sstop
+        if query not in border:
+            continue
+        qi, q = border[query]
+        ctg = q.seqid
+
+        bline = "\t".join(str(x) for x in (subject, sstart, sstop, ctg))
+        nbed.append(BedLine(bline))
+
+    nbed.sort(key=nbed.key)
+    nbed.print_to_file(fw=fw)
+    fw.close()
+
+
+def plot(args):
+    """
+    %prog plot scaffold.fasta synteny.bed physicalmap.bed
+
+    As evaluation of scaffolding, visualize external line of evidences:
+    * Plot synteny to an external genome
+    * Plot alignments to physical map
+    * Plot alignments to genetic map (TODO)
+
+    For example, `physicalmap.bed` can look like
+    scf7180014794474 104 104 ctg991
+
+    and will plot a dot in the dot plot in the corresponding location
+    the plots are one contig/scaffold per plot, but descend down the list in
+    scaffolds.sizes, until the contig is smaller than the cutoff
+    """
+    from jcvi.graphics.base import set_format
+
+    p = OptionParser(plot.__doc__)
+    p.add_option("--cutoff", dest="cutoff", default=1000000,
             help="plot for contigs and scaffolds > [default: %default]")
-    p.add_option("--format", dest="format", default="png",
-            help="generate image of format (png, pdf, ps, eps, svg, etc.)"
-            "[default: %default]")
+    set_format(p)
 
-    opts, args = p.parse_args()
+    opts, args = p.parse_args(args)
 
     if len(args) < 3:
-        sys.exit(p.print_help())
+        sys.exit(not p.print_help())
 
     scafsizes = Sizes(args[0])
     beds = args[1:]
 
     for scaffoldID, scafsize in scafsizes.iter_sizes():
+        if scafsize < opts.cutoff:
+            continue
         logging.debug("Loading {0} (size={1})".format(scaffoldID,
             thousands(scafsize)))
-        if scafsize < opts.cutoff:
-            break
         imagename = ".".join((scaffoldID, opts.format))
         plot_one_scaffold(scaffoldID, scafsize, beds, imagename)
+
+
+if __name__ == '__main__':
+    main()

f71e0e0bc25c2e35704bd455ddfce154ef4f5bbc
Vivek Krishnakumar <vkrishnakumar@jcvi.org>
2012-03-07 21:53:42 -0500

    add jcvi.format.ace to parse ace files. provide two options to extract contigs from ace file and prepare report of read location, consensus location or quality clip segments per contig

---
+++ formats/ace.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+Parse ace format files.
+"""
+
+import sys
+import logging
+
+from optparse import OptionParser
+from Bio.Sequencing import Ace
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+from jcvi.utils.iter import unique_everseen
+from jcvi.apps.base import ActionDispatcher, debug
+from jcvi.formats.base import must_open
+debug()
+
+
+def main():
+
+    actions = (
+        ('extract', 'extract contigs from ace file'),
+        ('report', 'generate report of read location, consensus location or quality segment per contig'),
+              )
+
+    p = ActionDispatcher(actions)
+    p.dispatch(globals())
+
+
+def extract(args):
+    """
+    %prog extract [--options] ace_file > fasta_file
+
+    Extract contigs from ace file and if necessary reformat header with
+    a pipe(|) separated list of constituent reads
+    """
+    p = OptionParser(extract.__doc__)
+    p.add_option("--format", default=False, action="store_true",
+            help="enable flag to reformat header into a symbol separated list of constituent reads [default: %default]")
+    p.add_option("--sep", default="|",
+            help="choose a separator used to list the reads in the FASTA header [default: '%default']")
+    opts, args = p.parse_args(args)
+
+    if len(args) != 1:
+        sys.exit(not p.print_help())
+    fw = sys.stdout
+
+    acefile, = args
+    ace = Ace.read(must_open(acefile))
+    logging.debug('Loaded ace file {0}'.format(acefile))
+
+    for c in ace.contigs:
+        id = c.name
+        if opts.format:
+           id = opts.sep.join([read.name for read in c.af])
+
+        seqrec = SeqRecord(Seq(c.sequence), id=id, description="")
+        SeqIO.write([seqrec], fw, "fasta")
+        fw.flush()
+
+def report(args):
+    """
+    %prog report [--options] ace_file > report
+
+    Prepare a report of read location, consensus location or quality segment per contig
+    """
+    from jcvi.utils.table import tabulate
+
+    p = OptionParser(report.__doc__)
+
+    types = {"read":      ["padded_start", "padded_end", "orient"],
+             "consensus": ["padded_consensus_start", "padded_consensus_end"],
+             "quality"  : ["qual_clipping_start", "qual_clipping_end", "align_clipping_start", "align_clipping_end"]
+            }
+    valid_types = tuple(types.keys())
+    p.add_option("--type", default="read", choices=valid_types,
+            help="choose report type [default: %default]")
+
+    opts, args = p.parse_args(args)
+
+    if len(args) != 1:
+        sys.exit(not p.print_help())
+
+    acefile, = args
+    ace = Ace.read(must_open(acefile))
+    logging.debug('Loaded ace file {0}'.format(acefile))
+
+    for c in ace.contigs:
+        print c.name
+        table = dict()
+        if opts.type == "read":
+            ps, pe = [], []
+            ps = [read.padded_start for read in c.af]
+            for i in xrange(1, len(ps)):
+                pe.append(ps[i] - ps[i-1])
+            pe.append(c.nbases)
+            map = dict(zip(ps, pe))
+            for i, read in enumerate(c.af):
+                values = [str(x) for x in (read.padded_start, map[read.padded_start], read.coru)]
+                for i, label in enumerate(types[opts.type]):
+                    table[(str(read.name), label)] = values[i]
+            print tabulate(table)
+        elif opts.type == "consensus":
+            for read in c.bs:
+                values = [str(x) for x in (read.padded_start, read.padded_end)]
+                for i, label in enumerate(types[opts.type]):
+                    table[(str(read.name), label)] = values[i]
+            print tabulate(table)
+        elif opts.type == "quality":
+            for read in c.reads:
+                (r1, r2) = (read.rd, read.qa)
+                values = [str(x) for x in (r2.qual_clipping_start, r2.qual_clipping_end, r2.align_clipping_start, r2.align_clipping_end)]
+                for i, label in enumerate(types[opts.type]):
+                    table[(str(r1.name), label)] = values[i]
+            print tabulate(table)
+        print
+    #logging.debug("Table with {0} rows written to `{1}`.".format(nrows, csvfile))
+
+
+if __name__ == '__main__':
+    main()

f35800b1d869ac66959717d356a61a17e0872e7b
Haibao Tang <tanghaibao@gmail.com>
2012-11-04 20:31:44 -0500

    add algorithms.reconstruct.adjgraph()

--- algorithms/reconstruct.py
+++ algorithms/reconstruct.py
@@ -26,9 +26,89 @@ def main():
         ('collinear', 'reduce synteny blocks to strictly collinear'),
         ('zipbed', 'build ancestral contig from collinear blocks'),
         ('pairs', 'convert anchorsfile to pairsfile'),
+        # Sankoff-Zheng reconstruction
+        ('adjgraph', 'construct adjacency graph'),
             )
     p = ActionDispatcher(actions)
     p.dispatch(globals())
+
+
+def adjgraph(args):
+    """
+    %prog adjgraph adjacency.txt subgraph.txt
+
+    Construct adjacency graph for graphviz. The file may look like sample below.
+    The lines with numbers are chromosomes with gene order information.
+
+    genome 0
+    chr 0
+    -1 -13 -16 3 4 -6126 -5 17 -6 7 18 5357 8 -5358 5359 -9 -10 -11 5362 5360
+    chr 1
+    138 6133 -5387 144 -6132 -139 140 141 146 -147 6134 145 -170 -142 -143
+    """
+    import pygraphviz as pgv
+    from jcvi.utils.iter import pairwise
+    from jcvi.formats.base import SetFile
+
+    p = OptionParser(adjgraph.__doc__)
+    opts, args = p.parse_args(args)
+
+    if len(args) != 2:
+        sys.exit(not p.print_help())
+
+    infile, subgraph = args
+    subgraph = SetFile(subgraph)
+    subgraph = set(x.strip("-") for x in subgraph)
+
+    G = pgv.AGraph(strict=False)  # allow multi-edge
+    SG = pgv.AGraph(strict=False)
+
+    palette = ("green", "magenta", "tomato", "peachpuff")
+    fp = open(infile)
+    genome_id = -1
+    key = 0
+    for row in fp:
+        if row.strip() == "":
+            continue
+
+        atoms = row.split()
+        tag = atoms[0]
+        if tag in ("ChrNumber", "chr"):
+            continue
+
+        if tag == "genome":
+            genome_id += 1
+            gcolor = palette[genome_id]
+            continue
+
+        nodeseq = []
+        for p in atoms:
+            np = p.strip("-")
+            nodeL, nodeR = np + "L", np + "R"
+            if p[0] == "-":  # negative strand
+                nodeseq += [nodeR, nodeL]
+            else:
+                nodeseq += [nodeL, nodeR]
+
+        for a, b in pairwise(nodeseq):
+            G.add_edge(a, b, key, color=gcolor)
+            key += 1
+
+            na, nb = a[:-1], b[:-1]
+            if na not in subgraph and nb not in subgraph:
+                continue
+
+            SG.add_edge(a, b, key, color=gcolor)
+
+    G.graph_attr.update(dpi="300")
+
+    fw = open("graph.dot", "w")
+    G.write(fw)
+    fw.close()
+
+    fw = open("subgraph.dot", "w")
+    SG.write(fw)
+    fw.close()
 
 
 def pairs(args):

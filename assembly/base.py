3eb6a4ce9f5d49d86f28c10ad7f24447d78eb98c
Haibao Tang <tanghaibao@gmail.com>
2013-10-16 16:36:40 -0400

    merge assembly.simulate into assembly.base

--- assembly/base.py
+++ assembly/base.py
@@ -7,6 +7,8 @@ Base utilties for genome assembly related calculations and manipulations
 
 import os.path as op
 import sys
+import logging
+
 from math import log
 ln2 = log(2)
 
@@ -122,6 +124,16 @@ def Astat(delta, k, G, n):
     return n * delta * 1. / G - k * ln2
 
 
+def main():
+
+    actions = (
+            ('n50', "Given FASTA or a list of contig sizes, calculate N50"),
+            ('wgsim', 'sample paired end reads using dwgsim'),
+            )
+    p = ActionDispatcher(actions)
+    p.dispatch(globals())
+
+
 def n50(args):
     """
     %prog n50 filename
@@ -176,13 +188,60 @@ def n50(args):
     return zip(header, summary)
 
 
-def main():
+def wgsim(args):
+    """
+    %prog wgsim fastafile
 
-    actions = (
-            ('n50', "Given FASTA or a list of contig sizes, calculate N50"),
-            )
-    p = ActionDispatcher(actions)
-    p.dispatch(globals())
+    Run dwgsim on fastafile.
+    """
+    p = OptionParser(wgsim.__doc__)
+    p.add_option("--erate", default=.02, type="float",
+                 help="Base error rate of the read [default: %default]")
+    p.add_option("--distance", default=500, type="int",
+                 help="Outer distance between the two ends [default: %default]")
+    p.add_option("--genomesize", type="int",
+                 help="Genome size in Mb [default: estimate from data]")
+    p.add_option("--readlen", default=100, type="int",
+                 help="Length of the read [default: %default]")
+    p.add_option("--noerrors", default=False, action="store_true",
+                 help="Simulate reads with no errors [default: %default]")
+    p.set_depth(depth=10)
+    opts, args = p.parse_args(args)
+
+    if len(args) != 1:
+        sys.exit(not p.print_help())
+
+    fastafile, = args
+    pf = fastafile.split(".")[0]
+
+    genomesize = opts.genomesize
+    size = genomesize * 1000000 if genomesize else Fasta(fastafile).totalsize
+    depth = opts.depth
+    readlen = opts.readlen
+    readnum = size * depth / (2 * readlen)
+
+    distance = opts.distance
+    stdev = distance / 5
+
+    outpf = "{0}.{1}bp.{2}x".format(pf, distance, depth)
+    distance -= 2 * readlen  # Outer distance => Inner distance
+    assert distance >= 0, "Outer distance must be >= 2 * readlen"
+
+    logging.debug("Total genome size: {0} bp".format(size))
+    logging.debug("Target depth: {0}x".format(depth))
+    logging.debug("Number of read pairs (2x{0}): {1}".format(readlen, readnum))
+
+    if opts.noerrors:
+        opts.erate = 0
+
+    cmd = "dwgsim -e {0} -E {0}".format(opts.erate)
+    if opts.noerrors:
+        cmd += " -r 0 -R 0 -X 0 -y 0"
+
+    cmd += " -d {0} -s {1}".format(distance, stdev)
+    cmd += " -N {0} -1 {1} -2 {1}".format(readnum, readlen)
+    cmd += " {0} {1}".format(fastafile, outpf)
+    sh(cmd)
 
 
 if __name__ == '__main__':

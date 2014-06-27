ec8b7924a3d556c83202c3444018ed1dfa3cda17
Haibao Tang <tanghaibao@gmail.com>
2014-06-27 07:50:24 -0700

    add graphics.image.rgb()

--- graphics/image.py
+++ graphics/image.py
@@ -8,14 +8,19 @@ Image processing pipelines for phenotyping projects.
 import os.path as op
 import sys
 import logging
+from math import sqrt
 
+import numpy as np
 import matplotlib.image as mpimg
-from jcvi.graphics.base import plt, savefig
+from matplotlib.colors import rgb2hex
+from jcvi.graphics.base import plt, savefig, normalize_axes, Rectangle
 
+from scipy import ndimage
 from skimage import filter, color, img_as_float
-from skimage.transform import hough_ellipse
+from skimage.transform import hough_ellipse, probabilistic_hough_line
 from skimage.draw import ellipse_perimeter
 from skimage import exposure
+from skimage.morphology import disk, closing
 from jcvi.apps.base import OptionParser, ActionDispatcher
 
 
@@ -23,9 +28,57 @@ def main():
 
     actions = (
         ('seeds', 'extract seed color from images'),
+        ('rgb', 'extract rgb from image'),
             )
     p = ActionDispatcher(actions)
     p.dispatch(globals())
+
+
+def rgb(args):
+    """
+    %prog rgb pngfile
+
+    Extract RGB from image.
+    """
+    p = OptionParser(rgb.__doc__)
+    opts, args, iopts = p.set_image_options(args)
+
+    if len(args) != 1:
+        sys.exit(not p.print_help())
+
+    pngfile, = args
+    pf = op.basename(pngfile).split(".")[0]
+    img = load_image(pngfile)
+
+    fig, (ax1, ax2) = plt.subplots(ncols=2, nrows=1, figsize=(6, 3))
+    ax1.set_title('Original picture')
+    ax1.imshow(img)
+
+    img_nonwhite = []
+    for row in img:
+        for r, g, b, a in row:
+            intensity = sqrt((r * r + g * g + b * b) / 3)
+            if intensity > .95:
+                continue
+            img_nonwhite.append((r, g, b))
+    rr, gg, bb = zip(*img_nonwhite)
+    rgb_1q = np.percentile(rr, 75), np.percentile(gg, 75), np.percentile(bb, 75)
+    rgb_m = np.median(rr), np.median(gg), np.median(bb)
+    rgb_3q = np.percentile(rr, 25), np.percentile(gg, 25), np.percentile(bb, 25)
+
+    yy = .6
+    for t, rgbx in zip(("1st quartile", "Median", "3rd quartile"),
+                    (rgb_1q, rgb_m, rgb_3q)):
+        ax2.text(.5, yy, t, ha="right", va="center")
+        ax2.add_patch(Rectangle((.6, yy - .04), .08, .08, lw=0, fc=rgbx))
+        hashtag = rgb2hex(rgbx).upper()
+        hashtag = hashtag.replace("#", "\#")
+        ax2.text(.7, yy, hashtag, va="center")
+        yy -= .1
+    normalize_axes(ax2)
+
+    image_name = pf + "." + iopts.format
+    savefig(image_name, dpi=iopts.dpi, iopts=iopts)
 
 
 def display_histogram(ax_hist, img, bins=256):
@@ -42,6 +95,13 @@ def display_histogram(ax_hist, img, bins=256):
     ax_cdf.set_yticks([])
 
 
+def load_image(pngfile):
+    img = mpimg.imread(pngfile)
+    w, h, c = img.shape
+    logging.debug("Image `{0}` loaded ({1}px x {2}px).".format(pngfile, w, h))
+    return img
+
+
 def seeds(args):
     """
     %prog seeds pngfile
@@ -56,9 +116,7 @@ def seeds(args):
 
     pngfile, = args
     pf = op.basename(pngfile).split(".")[0]
-    img = mpimg.imread(pngfile)
-    w, h, c = img.shape
-    logging.debug("Image `{0}` loaded ({1}px x {2}px).".format(pngfile, w, h))
+    img = load_image(pngfile)
     img = img[200:500, 100:400]
 
     fig, (ax1, ax2) = plt.subplots(ncols=2, nrows=1, figsize=(8, 4))
@@ -67,6 +125,11 @@ def seeds(args):
     #img_gray[img_gray < .2] = 1
     #edges = filter.sobel(img_gray)
     edges = filter.canny(img_gray)
+    #filled = ndimage.binary_fill_holes(edges)
+    #selem = disk(1)
+    #closed = closing(edges, selem)
+    lines = probabilistic_hough_line(edges, threshold=30,
+                                    line_length=30, line_gap=1)
 
     #result = hough_ellipse(edges, min_size=40, max_size=40)
     #print result
@@ -74,10 +137,17 @@ def seeds(args):
     ax1.imshow(img)
 
     ax2.set_title('Edge detection')
-    display_histogram(ax2, img_gray)
+    #display_histogram(ax2, img_gray)
     edges = color.gray2rgb(edges)
-    #img = color.gray2rgb(img_gray)
-    #ax2.imshow(edges)
+    w, h, c = edges.shape
+    #closed = color.gray2rgb(closed)
+    ax2.imshow(edges)
+    for p0, p1 in lines:
+        ax, ay = p0[0:2]
+        bx, by = p1[0:2]
+        plt.plot((ax, bx), (ay, by), "r-")
+    ax2.set_xlim(0, w)
+    ax2.set_ylim(h, 0)
 
     image_name = pf + "." + iopts.format
     savefig(image_name, dpi=iopts.dpi, iopts=iopts)

bd7e2dabd58a980ab331b8fa4713402a2926f592
Haibao Tang <tanghaibao@gmail.com>
2014-10-31 13:00:10 +0800

    add graphics.align.PairwiseAlign

--- graphics/align.py
+++ graphics/align.py
@@ -13,42 +13,66 @@ Illustrate three different types of alignments.
 
 import sys
 
+from jcvi.graphics.chromosome import Chromosome, HorizontalChromosome
 from jcvi.graphics.base import plt, savefig, normalize_axes
 from jcvi.apps.base import OptionParser
 
 
 class BaseAlign (object):
 
-    def __init__(self, ax, xmax=100):
-        self.ax = ax
+    def __init__(self, fig, xywh, xmax=100):
+        x, y, w, h = xywh
+        self.ax = fig.add_axes([x, y, w, h])
+        self.sax = fig.add_axes([x + .15 * w, y + .15 * h, w * .7, h * .7])
         self.amax = self.bmax = xmax
         self.a = [(0, xmax)]
         self.b = [(0, xmax)]
+        self.apatch = self.bpatch = None
+
+    def convert(self, pos, xmax):
+        return .15 + pos * .7 / xmax
 
     def invert(self, a, b):
         self.a = [(0, a), (a, b), (b, self.amax)]
         self.b = [(0, a), (b, a), (b, self.bmax)]
+        self.apatch = (self.convert(a, self.amax),
+                       self.convert(b, self.amax))
+        self.bpatch = (self.convert(a, self.bmax),
+                       self.convert(b, self.bmax))
 
     def delete(self, a, b):
         self.bmax -= b - a
         self.a = [(0, a), (b, self.amax)]
         self.b = [(0, a), (a, self.bmax)]
+        self.apatch = (self.convert(a, self.amax),
+                       self.convert(b, self.amax))
 
-    def duplicate(self, a, b):
-        self.bmax += b - a
+    def duplicate(self, a, b, gap=0):
+        self.bmax += b - a + gap
         self.a = [(0, b), (a, self.amax)]
-        self.b = [(0, b), (b, self.bmax)]
+        self.b = [(0, b), (b + gap, self.bmax)]
+        self.apatch = (self.convert(a, self.amax),
+                       self.convert(b, self.amax))
+        self.bpatch = (self.convert(a, self.bmax),
+                       self.convert(b, self.bmax),
+                       self.convert(b + gap, self.bmax),
+                       self.convert(2 * b - a + gap, self.bmax))
 
 
 class PairwiseAlign (BaseAlign):
 
-    def __init__(self, ax):
-        super(PairwiseAlign, self).__init__(ax)
+    def __init__(self, fig, xywh):
+        super(PairwiseAlign, self).__init__(fig, xywh)
 
-    def draw(self):
-        ax = self.ax
+    def draw(self, width=.03):
+        HorizontalChromosome(self.ax, .15, .85, .1, height=width,
+                             patch=self.apatch, lw=2)
+        Chromosome(self.ax, .1, .15, .85, width=width,
+                   patch=self.bpatch, lw=2)
         for a, b in zip(self.a, self.b):
-            ax.plot(a, b, "-", color="darkslategrey")
+            self.sax.plot(a, b, "-", color="darkslategrey", lw=2)
+        self.sax.set_xticklabels([])
+        self.sax.set_yticklabels([])
 
 
 def main():
@@ -63,9 +87,8 @@ def main():
 
     fig = plt.figure(1, (iopts.w, iopts.h))
     root = fig.add_axes([0, 0, 1, 1])
-    ax = fig.add_axes([.1, .1, .8, .8])
-    p = PairwiseAlign(ax)
-    p.invert(20, 30)
+    p = PairwiseAlign(fig, [0, 0, 1, 1])
+    p.duplicate(20, 30, gap=10)
     p.draw()
 
     normalize_axes(root)

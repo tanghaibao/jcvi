91a982c80b75541d24e94bc3f62a49b968698ee0
Haibao Tang <tanghaibao@gmail.com>
2015-12-17 19:48:35 -0800

    use exact match in hli.str

--- hli/str.py
+++ hli/str.py
@@ -5,16 +5,80 @@
 Identify repeat numbers in STR repeats.
 """
 
+import re
 import os.path as op
 import sys
 import logging
 import pysam
 
+from math import log
 from collections import defaultdict
 from jcvi.graphics.histogram import stem_leaf_plot
 from jcvi.utils.cbook import percentage
 from jcvi.apps.grid import MakeManager
 from jcvi.apps.base import OptionParser, ActionDispatcher, mkdir
+
+
+class STRLine(object):
+
+    def __init__(self, line):
+        args = line.split()
+        self.seqid = args[0]
+        self.start = int(args[1]) + 1
+        self.end = int(args[2])
+        self.period = int(args[4])
+        self.copynum = float(args[5])
+        self.consensusSize = int(args[6])
+        self.pctmatch = int(args[7])
+        self.pctindel = int(args[8])
+        self.score = int(args[9])
+        self.A = int(args[10])
+        self.C = int(args[11])
+        self.G = int(args[12])
+        self.T = int(args[13])
+        self.entropy = float(args[14])
+        self.motif = args[15]
+
+    def __str__(self):
+        return "\t".join(str(x) for x in (self.seqid, self.start, self.end,
+                        self.period, self.copynum, self.consensusSize,
+                        self.pctmatch, self.pctindel, self.score,
+                        self.A, self.C, self.G, self.T,
+                        "{0:.2f}".format(self.entropy), self.motif))
+
+    def is_valid(self, maxperiod=6, maxlength=150, minscore=50):
+        return self.period <= maxperiod and \
+               (self.end - self.start + 1) <= maxlength and \
+               self.score >= minscore
+
+    def calc_entropy(self):
+        total = self.A + self.C + self.G + self.T
+        fractions = [x * 1.0 / total for x in [self.A, self.C, self.G, self.T]]
+        entropy = sum([-1.0 * x * log(x, 2) for x in fractions if x != 0])
+        return entropy
+
+    def iter_exact_str(self, genome):
+        pat = re.compile("(({0}){{2,}})".format(self.motif))
+        start = self.start
+        s = genome[self.seqid][self.start - 1: self.end]
+        for m in re.finditer(pat, s):
+            self.start = start + m.start()
+            length = m.end() - m.start()
+            subseq = m.group(0)
+            assert length % len(self.motif) == 0
+            assert subseq.startswith(self.motif)
+
+            self.end = self.start - 1 + length
+            self.copynum = length / len(self.motif)
+            self.pctmatch = 100
+            self.pctindel = 0
+            self.score = 2 * length
+            self.A = subseq.count('A')
+            self.C = subseq.count('C')
+            self.G = subseq.count('G')
+            self.T = subseq.count('T')
+            self.entropy = self.calc_entropy()
+            yield self
 
 
 def main():
@@ -34,6 +98,8 @@ def lobstrindex(args):
     Make lobSTR index. Make sure the FASTA contain only upper case (so use
     fasta.format --upper to convert from UCSC fasta).
     """
+    import pyfasta
+
     p = OptionParser(lobstrindex.__doc__)
     p.set_home("lobstr")
     opts, args = p.parse_args(args)
@@ -46,23 +112,19 @@ def lobstrindex(args):
     pf = fastafile.split(".")[0]
     mkdir(pf)
 
+    genome = pyfasta.Fasta(fastafile)
     newbedfile = trfbed + ".new"
     newbed = open(newbedfile, "w")
     fp = open(trfbed)
     retained = total = 0
     for row in fp:
-        atoms = row.split()
-        start = int(atoms[1]) + 1
-        period = int(atoms[4])
-        copynum = int(float(atoms[5]))
-        length = period * copynum
+        s = STRLine(row)
         total += 1
-        if period > 6 or length > 150:  # lobSTR cannot type these loci
-            continue
-        retained += 1
-        end = start + length - 1
-        print >> newbed, "\t".join(str(x) for x in (atoms[0], start, end,
-                                period, copynum, "\t".join(atoms[6:])))
+        for ns in s.iter_exact_str(genome):
+            if not ns.is_valid():
+                continue
+            print >> newbed, ns
+            retained += 1
     newbed.close()
     logging.debug("Retained: {0}".format(percentage(retained, total)))
 

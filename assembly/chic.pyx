ba47279fdc1bd16b91d023df2f92021b62004785
Haibao Tang <tanghaibao@gmail.com>
2017-05-24 07:40:06 -0700

    [assembly] Fix bug in chic.pyx

--- assembly/chic.pyx
+++ assembly/chic.pyx
@@ -25,16 +25,20 @@ def score_evaluate(array.array tour,
     cdef int x
     cdef np.ndarray sizes_oo = \
                 np.array([tour_sizes[x] for x in tour])
-    cdef np.ndarray sizes_cum = np.cumsum(tour_sizes)
+    cdef np.ndarray sizes_cum = np.cumsum(sizes_oo) - sizes_oo // 2
 
     cdef double s = 0.0
     cdef int size = len(tour)
     cdef int a, b, links
     cdef int ia, ib
+    cdef int dist
     for ia in range(size):
         a = tour[ia]
         for ib in range(ia + 1, size):
             b = tour[ib]
             links = tour_M[a, b]
-            s += links / (sizes_cum[ib] - sizes_cum[ia])
+            dist = sizes_cum[ib] - sizes_cum[ia]
+            if dist > 1e7:
+                break
+            s += links / dist
     return s,

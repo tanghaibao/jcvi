53ee6613c49bf03769a1b9d0ae6727f9dccd51d6
Haibao Tang <tanghaibao@gmail.com>
2019-09-25 08:53:17 -0700

    [CI] Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@
 
 from __future__ import absolute_import
 
-import numpy as np
 import os.path as op
 import versioneer
 
 from setuptools import setup, find_packages, Extension
 from setup_helper import SetupHelper
-from Cython.Distutils import build_ext
 
 name = "jcvi"
 classifiers = [
@@ -24,25 +22,36 @@ classifiers = [
 # Use the helper
 h = SetupHelper(initfile="jcvi/__init__.py", readmefile="README.md")
 h.check_version(name, majorv=2, minorv=7)
+cmdclass = versioneer.get_cmdclass()
+include_dirs = []
 setup_dir = op.abspath(op.dirname(__file__))
 requirements = [x.strip() for x in open(op.join(setup_dir, "requirements.txt"))]
 h.install_requirements(requires=["cython", "numpy"])
-cmdclass = {"build_ext": build_ext}
-cmdclass.update(versioneer.get_cmdclass())
 
-# Now these are available
+# Build the ext
+try:
+    import numpy as np
+    from Cython.Distutils import build_ext
 
-# Start the show
+    cmdclass.update({"build_ext": build_ext})
+    include_dirs.append(np.get_include())
+except ImportError:
+    print("Cython not installed. Skip compiling Cython extensions.")
+
 ext_modules = [
     Extension(
         "jcvi.assembly.chic",
         ["jcvi/assembly/chic.pyx"],
-        include_dirs=[np.get_include()],
+        include_dirs=include_dirs,
         extra_compile_args=["-O3"],
     ),
     Extension(
         "jcvi.formats.cblast", ["jcvi/formats/cblast.pyx"], extra_compile_args=["-O3"]
     ),
+]
+
+packages = [name] + [
+    ".".join((name, x)) for x in find_packages("jcvi", exclude=["test*.py"])
 ]
 
 setup(
@@ -54,8 +63,7 @@ setup(
     long_description=h.long_description,
     long_description_content_type="text/markdown",
     cmdclass=cmdclass,
-    packages=[name]
-    + [".".join((name, x)) for x in find_packages("jcvi", exclude=["test*.py"])],
+    packages=packages,
     include_package_data=True,
     package_data={"jcvi.utils.data": ["*.*"]},
     ext_modules=ext_modules,
@@ -64,5 +72,6 @@ setup(
     url="http://github.com/tanghaibao/jcvi",
     description="Python utility libraries on genome assembly, "
     "annotation and comparative genomics",
+    setup_requires=["setuptools>=18.0", "cython"],
     install_requires=requirements,
 )

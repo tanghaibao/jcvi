cb3e1cbfe1f4eb79e1e7aed60685adf536bb7dbc
Haibao Tang <tanghaibao@gmail.com>
2020-06-16 23:21:11 -0700

    [formats] Reformat obo

--- jcvi/formats/obo.py
+++ jcvi/formats/obo.py
@@ -23,7 +23,7 @@ def after_colon(line):
     return line.split(":", 1)[1].strip()
 
 
-class OBOReader (object):
+class OBOReader(object):
     """
     parse obo file, usually the most updated can be downloaded from
     http://www.geneontology.org/ontology/obo_format_1_2/gene_ontology.1_2.obo
@@ -39,8 +39,10 @@ class OBOReader (object):
             self._handle = open(obo_file)
         except IOError as e:
             logging.error(e)
-            logging.error("download obo first: " +
-                          "http://geneontology.org/ontology/obo_format_1_2/")
+            logging.error(
+                "download obo first: "
+                + "http://geneontology.org/ontology/obo_format_1_2/"
+            )
             sys.exit(1)
 
     def __iter__(self):
@@ -79,34 +81,37 @@ class OBOReader (object):
                 rec.namespace = after_colon(line)
             elif line.startswith("is_a:"):
                 rec._parents.append(after_colon(line).split()[0])
-            elif line.startswith("is_obsolete:") \
-                    and after_colon(line) == "true":
+            elif line.startswith("is_obsolete:") and after_colon(line) == "true":
                 rec.is_obsolete = True
 
         return rec
 
 
-class GOTerm (object):
+class GOTerm(object):
     """
     GO term, actually contain a lot more properties than interfaced here
     """
 
     def __init__(self):
-        self.id = ""              # GO:xxxxxx
-        self.name = ""            # description
-        self.namespace = ""       # BP, CC, MF
-        self._parents = []        # is_a string of parents
-        self.parents = []         # parent records
-        self.children = []        # children records
-        self.level = -1           # distance from root node
+        self.id = ""  # GO:xxxxxx
+        self.name = ""  # description
+        self.namespace = ""  # BP, CC, MF
+        self._parents = []  # is_a string of parents
+        self.parents = []  # parent records
+        self.children = []  # children records
+        self.level = -1  # distance from root node
         self.is_obsolete = False  # is_obsolete
-        self.alt_ids = []         # alternative identifiers
+        self.alt_ids = []  # alternative identifiers
 
     def __str__(self):
         obsolete = "obsolete" if self.is_obsolete else ""
-        return "%s\tlevel-%02d\t%s [%s] %s" % \
-            (self.id, self.level, self.name,
-             self.namespace, obsolete)
+        return "%s\tlevel-%02d\t%s [%s] %s" % (
+            self.id,
+            self.level,
+            self.name,
+            self.namespace,
+            obsolete,
+        )
 
     def __repr__(self):
         return "GOTerm('%s')" % (self.id)
@@ -153,7 +158,6 @@ class GOTerm (object):
 
 
 class GODag(dict):
-
     def __init__(self, obo_file="gene_ontology.1_2.obo"):
 
         self.load_obo_file(obo_file)
@@ -172,7 +176,6 @@ class GODag(dict):
         logging.debug("%d nodes imported" % len(self))
 
     def populate_terms(self):
-
         def depth(rec):
             if rec.level < 0:
                 if not rec.parents:
@@ -212,13 +215,18 @@ class GODag(dict):
         return rec
 
     def _label_wrap(self, label):
-        wrapped_label = r"%s\n%s" % (label,
-                                     self[label].name.replace(",", r"\n"))
+        wrapped_label = r"%s\n%s" % (label, self[label].name.replace(",", r"\n"))
         return wrapped_label
 
-    def draw_lineage(self, recs, nodecolor="mediumseagreen",
-                     edgecolor="lightslateblue", dpi=96, verbose=False,
-                     lineage_img="GO_lineage.png"):
+    def draw_lineage(
+        self,
+        recs,
+        nodecolor="mediumseagreen",
+        edgecolor="lightslateblue",
+        dpi=96,
+        verbose=False,
+        lineage_img="GO_lineage.png",
+    ):
         # draw AMIGO style network, lineage containing one query record
         import pygraphviz as pgv
 
@@ -228,8 +236,7 @@ class GODag(dict):
             edgeset.update(rec.get_all_parent_edges())
             edgeset.update(rec.get_all_child_edges())
 
-        edgeset = [(self._label_wrap(a), self._label_wrap(b))
-                   for (a, b) in edgeset]
+        edgeset = [(self._label_wrap(a), self._label_wrap(b)) for (a, b) in edgeset]
 
         for src, target in edgeset:
             """
@@ -239,10 +246,10 @@ class GODag(dict):
             G.add_edge(target, src)
 
         G.graph_attr.update(dpi="%d" % dpi)
-        G.node_attr.update(shape="box", style="rounded,filled",
-                           fillcolor="beige", color=nodecolor)
-        G.edge_attr.update(shape="normal", color=edgecolor,
-                           dir="back", label="is_a")
+        G.node_attr.update(
+            shape="box", style="rounded,filled", fillcolor="beige", color=nodecolor
+        )
+        G.edge_attr.update(shape="normal", color=edgecolor, dir="back", label="is_a")
         # highlight the query terms
         for rec in recs:
             try:
@@ -254,8 +261,11 @@ class GODag(dict):
         if verbose:
             print(G.to_string(), file=sys.stderr)
 
-        print("lineage info for terms %s written to %s" %
-              ([rec.id for rec in recs], lineage_img), file=sys.stderr)
+        print(
+            "lineage info for terms %s written to %s"
+            % ([rec.id for rec in recs], lineage_img),
+            file=sys.stderr,
+        )
 
         G.draw(lineage_img, prog="dot")
 
@@ -311,12 +321,17 @@ def validate_term(term, so=None, method="verify"):
     return term
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     import optparse
+
     p = optparse.OptionParser(__doc__)
-    p.add_option("--term", dest="term", help="write the parents and children"
-                 "of the query term", default=None)
+    p.add_option(
+        "--term",
+        dest="term",
+        help="write the parents and children of the query term",
+        default=None,
+    )
 
     opts, args = p.parse_args()
 

f74c654bc98647a9fc20812a49b13a9a5e1d34da
Haibao Tang <tanghaibao@gmail.com>
2020-06-17 10:07:23 -0700

    [formats] Revert deletion of load_Dag()

--- jcvi/formats/obo.py
+++ jcvi/formats/obo.py
@@ -9,9 +9,51 @@ Parses obo_file and plot GO lineage
 from __future__ import print_function
 
 import sys
+import logging
+
+from collections import deque
 
 from goatools.obo_parser import GODag
 from jcvi.apps.base import OptionParser
+
+
+def load_GODag():
+    """
+    OBO file retrieved from http://obo.cvs.sourceforge.net/viewvc/obo/obo/ontology/genomic-proteomic/so.obo
+    """
+    from jcvi.apps.base import download
+
+    so_file_url = "http://obo.cvs.sourceforge.net/viewvc/obo/obo/ontology/genomic-proteomic/so.obo"
+    so_file = download(so_file_url, debug=False)
+
+    return GODag(so_file)
+
+
+def validate_term(term, so=None, method="verify"):
+    """
+    Validate an SO term against so.obo
+    """
+    if so is None:
+        so = load_GODag()
+
+    oterm = term
+    valid_names = set(x.name for x in so.values())
+    if term not in so.valid_names:
+        if "resolve" in method:
+            if "_" in term:
+                tparts = deque(term.split("_"))
+                tparts.pop() if "prefix" in method else tparts.popleft()
+                nterm = "_".join(tparts).strip()
+                term = validate_term(nterm, so=so, method=method)
+            if term is None:
+                return None
+        else:
+            logging.error("Term `{0}` does not exist".format(term))
+            sys.exit(1)
+
+    if oterm != term:
+        logging.debug("Resolved term `{0}` to `{1}`".format(oterm, term))
+    return term
 
 
 if __name__ == "__main__":

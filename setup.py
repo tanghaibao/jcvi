085d9a8a8d445a0a2c3b2383aab8844b2e8120ca
Haibao Tang <tanghaibao@gmail.com>
2022-11-26 11:27:51 -0800

    Add Cython and numpy to build

--- setup.py
+++ setup.py
@@ -2,7 +2,25 @@
 
 """Package setup"""
 
-import setuptools
+import numpy as np
+import versioneer
+
+from Cython.Build import build_ext
+from setuptools import setup, Extension
+
+cmdclass = versioneer.get_cmdclass()
+cmdclass.update({"build_ext": build_ext})
+ext_modules = [
+    Extension(
+        "jcvi.assembly.chic",
+        ["jcvi/assembly/chic.pyx"],
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-O3"],
+    ),
+    Extension(
+        "jcvi.formats.cblast", ["jcvi/formats/cblast.pyx"], extra_compile_args=["-O3"]
+    ),
+]
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setup(cmdclass=cmdclass, ext_modules=ext_modules)

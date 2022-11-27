0726c3941cf3266d2a6cf582058546629a7205f3
Haibao Tang <tanghaibao@gmail.com>
2022-11-27 07:23:39 -0800

    Remove unused deps (#528)
    * Remove unused deps
    
    * PyYAML used in tests
    
    * Add PyYAML in build.yml

--- tests/assembly/test_allmaps.py
+++ tests/assembly/test_allmaps.py
@@ -1,10 +1,15 @@
 import os.path as op
 
 
+def datafile(path: str) -> str:
+    """Returns the path to a file in the designated test directory."""
+    return op.join(op.dirname(__file__), "allmaps.py", path)
+
+
 def test_weights():
     from jcvi.assembly.allmaps import Weights
 
-    weightsfile = op.join(op.dirname(__file__), "weights.txt")
+    weightsfile = datafile("inputs/weights.txt")
     weights = Weights(weightsfile, ["JMMale", "JMFemale"])
     assert weights.maps == ["JMMale", "JMFemale"]
 
@@ -17,12 +22,10 @@ def test_liftover():
     from jcvi.assembly.allmaps import liftover
     from ..config import compare_line_by_line
 
-    chainfile = op.join(op.dirname(__file__), "allmaps.py", "inputs", "JM-2.chain")
-    bedfile = op.join(op.dirname(__file__), "allmaps.py", "inputs", "JM-2.bed")
+    chainfile = datafile("inputs/JM-2.chain")
+    bedfile = datafile("inputs/JM-2.bed")
     liftedbedfile = "JM-2.lifted.bed"
-    expected = op.join(
-        op.dirname(__file__), "allmaps.py", "references", "JM-2.lifted.bed"
-    )
+    expected = datafile("references/JM-2.lifted.bed")
     liftover(chainfile, bedfile, liftedbedfile, unmapfile="unmapped", cstyle="l")
     compare_line_by_line(liftedbedfile, expected)
     cleanup(liftedbedfile, "unmapped")

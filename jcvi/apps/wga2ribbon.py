bf49be001afe84a6c5023dc55de3ca0b823e11fb
Adam Taranto <adam.p.taranto@gmail.com>
2023-02-21 10:26:44 +1100

    Black formatting

--- jcvi/apps/wga2ribbon.py
+++ jcvi/apps/wga2ribbon.py
@@ -6,47 +6,71 @@ Convert PAF/LASTZ alignments to bed and block files for jcvi.ribbon
 using layout file as guide.
 """
 
-import os
-import sys
-import glob
-import shutil
-import argparse
 from collections import namedtuple
-from operator import attrgetter
 from jcvi.graphics.base import AbstractLayout
+from operator import attrgetter
+import argparse
+import glob
+import os
+import shutil
+import sys
 
 
 def mainArgs():
-    '''Parse command line arguments.'''
+    """Parse command line arguments."""
     parser = argparse.ArgumentParser(
-        description='Convert whole-genome alignments to block & bed format \
-                    for ribbon.',
-        prog='jvci.apps.wga2ribbon')
-    parser.add_argument('-f', '--format', type=str, required=True,
-                        choices=['paf', 'lastz'],
-                        help='Set alignment format as paf or lastz.')
-    parser.add_argument('-a', '--alignments', action='append', required=True,
-                        help='Set arg for each alignment file to be imported.\
+        description="Convert whole-genome alignments to block & bed format \
+                    for ribbon.",
+        prog="jvci.apps.wga2ribbon",
+    )
+    parser.add_argument(
+        "-f",
+        "--format",
+        type=str,
+        required=True,
+        choices=["paf", "lastz"],
+        help="Set alignment format as paf or lastz.",
+    )
+    parser.add_argument(
+        "-a",
+        "--alignments",
+        action="append",
+        required=True,
+        help='Set arg for each alignment file to be imported.\
                         Has format "[Source chrm Idx],[Hit chrm Idx],\
                         [filename]" i.e. For file with alignmnets between the \
                         first and second tracks in ribbon.layout: \
-                        -a "0,1,A2B_Alignment.tab"')
-    parser.add_argument('-l', '--layout', type=str, required=True,
-                        help='Ribbon plot layout file.')
-    parser.add_argument('--minID', type=float, default=50,
-                        help='Extract alignments with identity => than minID')
-    parser.add_argument('--outBed', type=str, default='ribbons.bed',
-                        help='Write alignment block coords to this bedfile')
-    parser.add_argument('--outBlocks', type=str, default='ribbons.block',
-                        help='Write alignment block names to this file.')
+                        -a "0,1,A2B_Alignment.tab"',
+    )
+    parser.add_argument(
+        "-l", "--layout", type=str, required=True, help="Ribbon plot layout file."
+    )
+    parser.add_argument(
+        "--minID",
+        type=float,
+        default=50,
+        help="Extract alignments with identity => than minID",
+    )
+    parser.add_argument(
+        "--outBed",
+        type=str,
+        default="ribbons.bed",
+        help="Write alignment block coords to this bedfile",
+    )
+    parser.add_argument(
+        "--outBlocks",
+        type=str,
+        default="ribbons.block",
+        help="Write alignment block names to this file.",
+    )
     args = parser.parse_args()
     return args
 
 
-class LayoutLine (object):
+class LayoutLine(object):
     #  0-11: x,y,rotation,ha,va,color,ratio,label,chrmName,rStart,rEnd,chrmMax
-    def __init__(self, row, delimiter=','):
-        self.hidden = row[0] == '*'
+    def __init__(self, row, delimiter=","):
+        self.hidden = row[0] == "*"
         if self.hidden:
             row = row[1:]
         args = row.rstrip().split(delimiter)
@@ -78,9 +102,8 @@ class LayoutLine (object):
             self.chrmMax = None
 
 
-class Layout (AbstractLayout):
-
-    def __init__(self, filename, delimiter=','):
+class Layout(AbstractLayout):
+    def __init__(self, filename, delimiter=","):
         super(Layout, self).__init__(filename)
         fp = open(filename)
         self.edges = []
@@ -88,16 +111,16 @@ class Layout (AbstractLayout):
             # Skip blank lines
             if row.strip():
                 # Skip header rows
-                if row[0] == '#':
+                if row[0] == "#":
                     continue
                 # Import edges
-                if row[0] == 'e':
+                if row[0] == "e":
                     args = row.rstrip().split(delimiter)
                     args = [x.strip() for x in args]
                     # From index 1 and 2
                     a, b = args[1:3]
                     a, b = int(a), int(b)
-                    assert args[0] == 'e'
+                    assert args[0] == "e"
                     self.edges.append((a, b))
                 else:
                     # Parse all other lines as sequence tracks
@@ -111,19 +134,30 @@ class Layout (AbstractLayout):
 
 
 def readLASTZ(infile, minID=50):
-    '''
+    """
     Read in LASTZ result file from LASTZ_genome_align.sh
     Populate nested dictionary of hits keyed by Target
     and then Query scaffold names.
     # Check diff between standard LASTZ and mimeo-LASTZ formats
-    '''
+    """
     with open(infile) as f:
         content = f.readlines()
     content = [x.strip().split() for x in content]
     hitsDict = dict()
     # Set named tuple format
-    hitTup = namedtuple('Elem', ['t_start', 't_end', 't_strand', 'q_start',
-                                 'q_end', 'q_strand', 'idPct', 'UID'])
+    hitTup = namedtuple(
+        "Elem",
+        [
+            "t_start",
+            "t_end",
+            "t_strand",
+            "q_start",
+            "q_end",
+            "q_strand",
+            "idPct",
+            "UID",
+        ],
+    )
     counter = 0
     # Read in split rows
     for row in content:
@@ -158,12 +192,14 @@ def readLASTZ(infile, minID=50):
             if q_name not in list(hitsDict[t_name].keys()):
                 hitsDict[t_name][q_name] = list()
             # Write record to target:query list as named tuple
-            hitsDict[t_name][q_name].append(hitTup(t_start,t_end,t_strand,q_start,q_end,q_strand,idPct,UID))
+            hitsDict[t_name][q_name].append(
+                hitTup(t_start, t_end, t_strand, q_start, q_end, q_strand, idPct, UID)
+            )
     return hitsDict
 
 
 def readPAF(infile, minID=50):
-    '''
+    """
     Format info here: https://github.com/lh3/miniasm/blob/master/PAF.md
     From PAF file populate nested dictionary of hits keyed by Target (Ref) and
     then Query scaffold names.
@@ -172,7 +208,7 @@ def readPAF(infile, minID=50):
     # Check if ribbon wants intervals indexed from zero
     # PAF is zero based
     # Check how strand orientation is assigned
-    '''
+    """
     # |Col|Type  |Description                               |
     # |--:|:----:|:-----------------------------------------|
     # |0  |string|Query sequence name                       |
@@ -192,8 +228,19 @@ def readPAF(infile, minID=50):
     content = [x.strip().split() for x in content]
     hitsDict = dict()
     # Set named tuple format
-    hitTup = namedtuple('Elem', ['t_start', 't_end', 't_strand', 'q_start',
-                                 'q_end', 'q_strand', 'idPct', 'UID'])
+    hitTup = namedtuple(
+        "Elem",
+        [
+            "t_start",
+            "t_end",
+            "t_strand",
+            "q_start",
+            "q_end",
+            "q_strand",
+            "idPct",
+            "UID",
+        ],
+    )
     counter = 0
     # Read in split rows
     for row in content:
@@ -202,7 +249,7 @@ def readPAF(infile, minID=50):
             continue
         # Count if hit identity exceeds threshold
         # For PAF calc idenity from matching bases and alignment length.
-        rowID = float(row[9])/float(row[10])
+        rowID = float(row[9]) / float(row[10])
         if rowID >= minID:
             counter += 1
             UID = counter
@@ -229,14 +276,9 @@ def readPAF(infile, minID=50):
             if q_name not in list(hitsDict[t_name].keys()):
                 hitsDict[t_name][q_name] = list()
             # Write record to target:query list as named tuple
-            hitsDict[t_name][q_name].append(hitTup(t_start,
-                                                   t_end,
-                                                   t_strand,
-                                                   q_start,
-                                                   q_end,
-                                                   q_strand,
-                                                   idPct,
-                                                   UID))
+            hitsDict[t_name][q_name].append(
+                hitTup(t_start, t_end, t_strand, q_start, q_end, q_strand, idPct, UID)
+            )
     return hitsDict
 
 
@@ -257,60 +299,83 @@ def main():
     # Init blocklines
     blocklines = list()
     for coords in args.alignments:
-        assert len(coords.split(',')) == 3
-        selfIdx, hitIdx, alnFile = coords.split(',')
+        assert len(coords.split(",")) == 3
+        selfIdx, hitIdx, alnFile = coords.split(",")
         selfIdx = int(selfIdx)
         hitIdx = int(hitIdx)
         hits = readLASTZ(alnFile, minID=args.minID)
         # Extract hits between sequence selfIdx and hitIdx as per layout file.
-        for x in sorted(hits[plotlines[selfIdx].ChrmName][plotlines[hitIdx].ChrmName], key = lambda x: (int(x.t_start), int(x.t_end))):
+        for x in sorted(
+            hits[plotlines[selfIdx].ChrmName][plotlines[hitIdx].ChrmName],
+            key=lambda x: (int(x.t_start), int(x.t_end)),
+        ):
             tName = str(plotlines[selfIdx].ChrmName)
             qName = str(plotlines[hitIdx].ChrmName)
-            tUID = '_'.join([str(selfIdx),
-                             str(hitIdx),
-                             str(selfIdx),
-                             'ID'+str(x.UID),
-                             tName,
-                             str(x.t_start),
-                             str(x.t_end)])
-            qUID = '_'.join([str(selfIdx),
-                             str(hitIdx),
-                             str(hitIdx),
-                             'ID'+str(x.UID),
-                             qName,
-                             str(x.q_start),
-                             str(x.q_end)])
-            bedlines[selfIdx].append(tuple([tName,
-                                            str(x.t_start),
-                                            str(x.t_end),
-                                            tUID,
-                                            str(x.idPct),
-                                            str(x.t_strand)]))
-            bedlines[hitIdx].append(tuple([qName,
-                                           str(x.q_start),
-                                           str(x.q_end),
-                                           qUID,
-                                           str(x.idPct),
-                                           str(x.q_strand)]))
-            blockline = ['.'] * plotlines.lines
+            tUID = "_".join(
+                [
+                    str(selfIdx),
+                    str(hitIdx),
+                    str(selfIdx),
+                    "ID" + str(x.UID),
+                    tName,
+                    str(x.t_start),
+                    str(x.t_end),
+                ]
+            )
+            qUID = "_".join(
+                [
+                    str(selfIdx),
+                    str(hitIdx),
+                    str(hitIdx),
+                    "ID" + str(x.UID),
+                    qName,
+                    str(x.q_start),
+                    str(x.q_end),
+                ]
+            )
+            bedlines[selfIdx].append(
+                tuple(
+                    [
+                        tName,
+                        str(x.t_start),
+                        str(x.t_end),
+                        tUID,
+                        str(x.idPct),
+                        str(x.t_strand),
+                    ]
+                )
+            )
+            bedlines[hitIdx].append(
+                tuple(
+                    [
+                        qName,
+                        str(x.q_start),
+                        str(x.q_end),
+                        qUID,
+                        str(x.idPct),
+                        str(x.q_strand),
+                    ]
+                )
+            )
+            blockline = ["."] * plotlines.lines
             blockline[selfIdx] = tUID
             blockline[hitIdx] = qUID
-            blocklines.append('\t'.join(blockline))
+            blocklines.append("\t".join(blockline))
 
     # Sort bedlines for each seq in layout file.
     # Sort by Chrm name, start, end, UID
-    with open(args.outBed, 'w') as f:
+    with open(args.outBed, "w") as f:
         for layoutIdx in list(bedlines.keys()):
-            for line in sorted(bedlines[layoutIdx],
-                               key=lambda x:
-                                   (x[0], int(x[1]), int(x[2]), x[3])):
-                f.write('\t'.join(line) + '\n')
+            for line in sorted(
+                bedlines[layoutIdx], key=lambda x: (x[0], int(x[1]), int(x[2]), x[3])
+            ):
+                f.write("\t".join(line) + "\n")
 
     # Write blocklines
-    with open(args.outBlocks, 'w') as f:
+    with open(args.outBlocks, "w") as f:
         for line in blocklines:
-            f.write(line + '\n')
+            f.write(line + "\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

067d185863959be9284b6d625b45a632fe470646
Haibao Tang <tanghaibao@gmail.com>
2024-04-30 20:48:34 -0700

    revert logging in webcolors

--- jcvi/utils/webcolors.py
+++ jcvi/utils/webcolors.py
@@ -7,13 +7,12 @@
 # Created by Haibao Tang on 01/28/20
 # Copyright © 2021 Haibao Tang. All rights reserved.
 #
+import logging
 
 import numpy as np
 
 from skimage.color import rgb2lab, deltaE_cmc
 from webcolors import CSS3_HEX_TO_NAMES, hex_to_rgb
-
-from ..apps.base import logger
 
 
 def color_diff(rgb1, rgb2):
@@ -37,12 +36,12 @@ def closest_color(requested_color):
     """
     Find closest color name for the request RGB tuple.
     """
-    logger.disable(logger.DEBUG)
+    logging.disable(logging.DEBUG)
     colors = []
     for key, name in CSS3_HEX_TO_NAMES.items():
         diff = color_diff(hex_to_rgb(key), requested_color)
         colors.append((diff, name))
-    logger.disable(logger.NOTSET)
+    logging.disable(logging.NOTSET)
     _, min_color = min(colors)
 
     return min_color

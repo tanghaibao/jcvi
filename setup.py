e5be70067c6559c45d4b7d550809fb689dacf10d
Adam Taranto <adam.p.taranto@gmail.com>
2025-01-03 14:08:13 +1100

    Use setuptools for cython modules

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""Package setup for Cython extensions only"""
+
+from Cython.Build import build_ext
+from setuptools import setup, Extension
+import numpy as np
+
+ext_modules = [
+    Extension(
+        "jcvi.assembly.chic",
+        ["src/jcvi/assembly/chic.pyx"],
+        include_dirs=[np.get_include()],
+        extra_compile_args=["-O3"],
+    ),
+    Extension(
+        "jcvi.formats.cblast", 
+        ["src/jcvi/formats/cblast.pyx"], 
+        extra_compile_args=["-O3"]
+    ),
+]
+
+setup(
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+)

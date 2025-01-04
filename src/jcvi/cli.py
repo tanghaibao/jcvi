90ebc723ec48934631a9031bca6707009f12a9ef
Adamtaranto <Adamtaranto@users.noreply.github.com>
2025-01-04 08:38:34 +0000

    Style fixes by Black

--- src/jcvi/cli.py
+++ src/jcvi/cli.py
@@ -2,11 +2,14 @@
 import argparse
 from . import __version__
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--version', action='version', 
-                       version=f'%(prog)s {__version__}')
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {__version__}"
+    )
     args = parser.parse_args()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

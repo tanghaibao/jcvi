5de54154abe509fae2901eca4b5f9db0e9078536
Adam Taranto <adam.p.taranto@gmail.com>
2025-05-11 12:57:12 +1000

    missing import

--- src/jcvi/formats/obo.py
+++ src/jcvi/formats/obo.py
@@ -9,7 +9,7 @@ Parses obo_file and plot GO lineage
 from collections import deque
 from functools import partial
 import sys
-from typing import IO, Optional
+from typing import IO, Optional, Tuple
 
 from goatools.obo_parser import GODag
 
